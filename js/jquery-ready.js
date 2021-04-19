bdb01482b89b064f7c85ff060ad4f5997f4832c8
vlad_z <vzv@siluetstudio.com>
2021-04-19 13:27:31 +0300

    adaptive all page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -22,6 +22,21 @@ $(document).ready(function() {
         });
     }; 
 
+    //добавление поиска в мобильное меню
+    if (window.innerWidth <= 992) {
+        $('.form.form_search').appendTo('.header__bottom .header__wrap');
+    }
+
+    //клик по бургеру
+    $('.burger').click(function() {
+        $('.header__bottom').show("slide", { direction: "right" }, 500);
+    });
+
+    //закрытие меню
+    $('.header__bottom .close').click(function() {
+        $('.header__bottom').hide("slide", { direction: "right" }, 500);
+    })
+
 
     //одинаковая высота новостей
     if($('.news').not('.news_detail').length) {
@@ -32,4 +47,50 @@ $(document).ready(function() {
     if($('.advantages').length) {
         $('.advantages__item .advantages__text').matchHeight();
     }
+
+    //одинаковая высота названий контактов
+    if($('.contact_page').length) {
+        $('.contact__row .contact__item-name').matchHeight();
+    }
+
+    //аккордион
+    if(('.js-accordion').length) {
+        $('.js-accordion:first-of-type').addClass('active').find('.js-accordion__content').slideDown();
+        if(!$('.catalog_detail').length) {
+            $('.catalog__menu .catalog__menu-item.current').find('.catalog__menu-drop__item:first-of-type').addClass('active');
+        }
+
+        $(".js-accordion").find('.js-accordion__toggle').click(function() {
+            $(this).closest('.js-accordion').addClass('active').siblings().removeClass('active').find('.js-accordion__content').slideUp();
+            $(this).closest('.js-accordion').find('.js-accordion__content').slideDown();
+
+            if(!$('.catalog_detail').length) {
+                $('.catalog__menu .catalog__menu-item.current').find('.catalog__menu-drop__item').removeClass('active').eq($(this).closest('.js-accordion').index()).addClass('active');
+            }
+        })
+    }
+
+    //сайдбар в каталоге
+    if(!$('.catalog_detail').length && $('.catalog_page').length) {
+        $('.catalog__menu .catalog__menu-item.current').find('.catalog__menu-drop__item').click(function() {
+            if(!$(this).hasClass('active')) {
+                $(this).addClass('active').siblings().removeClass('active');
+                $('.js-accordion').removeClass('active').find('.js-accordion__content').slideUp();
+                $('.js-accordion').eq($(this).index()).addClass('active').find('.js-accordion__content').slideDown();
+            }            
+        })
+    }
+
+    if($('.catalog_detail').length) {
+        $('.catalog__side .catalog__menu-drop__item').removeClass('active')
+    }
+
+    $('.catalog__menu-toggle').click(function() {
+        if(!$(this).closest('.catalog__menu-item').hasClass('current')) {
+            $(this).closest('.catalog__menu-item').toggleClass('active').siblings().not('.current').removeClass('active').find('.catalog__menu-drop').slideUp();
+            
+            $(this).siblings('.catalog__menu-drop').slideToggle();
+        }
+    })
+    
 });
